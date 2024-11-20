4693923b222a4296d18e2f84921624f3bf36c6cf
Bogdan Caraeane <bogdan.c.2206@gmail.com>
2024-11-20 14:17:38 +0200

    Am adaugat scriptul pentru verificarea adrselor IP modificat pentru ainclude o functie shell

--- check_ip.sh
+++ check_ip.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-grep -E "^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" /etc/hosts
+function check_ip {
+	local host=$1
+	local ip=$2
+	local dns_server=$3
+	nslookup $host $dns_server | grep -q "$ip"
+	if [ $? -eq 0 ]; then
+		echo "Adresa IP valida pentru $hosts"
+	else
+		echo "Adresa IP invalida pentru $hosts"
+	fi
+check_ip $1 $2 $3
